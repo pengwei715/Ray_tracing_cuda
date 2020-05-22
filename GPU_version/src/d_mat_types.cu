4f135e2ba93055de8c102cb307d58cbebba8e781
Peng Wei <pengwei715@gmail.com>
2020-05-22 00:15:59 -0500

    clean up

--- GPU_version/src/d_mat_types.cu
+++
@@ -1,9 +0,0 @@
-#include "d_mat_types.h"
-
-
-__device__ float getElement(const d_MatFloat A, int row, int col){
-  return A.data[row*A.stride + col];
-}
-__device__ void setElement(d_MatFloat A, int row, int col, float val){
-  A.data[row*A.stride + col] = val;
-}

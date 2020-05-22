fafc0e311e689b086b3f45a7b8d2ed93970e8008
Peng Wei <pengwei@cs.uchicago.edu>
2020-05-22 03:52:49 -0500

    update the result

--- plot.py
+++ plot.py
@@ -1,7 +1,11 @@
 import pandas as pd
-import matplotlib as plt
-
+import matplotlib.pyplot as plt
+import numpy as np
 df = pd.read_csv('res.csv')
-xs=df['grid_dim']
+xs=np.log(df['grid_dim'])
 ys=df['time']
 plt.plot(xs,ys,"-o")
+plt.title("Different grid size performance")
+plt.xlabel("grid_size")
+plt.ylabel("time")
+plt.savefig("grid.png")
